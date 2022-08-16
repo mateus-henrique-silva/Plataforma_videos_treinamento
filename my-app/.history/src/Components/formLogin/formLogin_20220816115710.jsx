e1b1059992a723a5dc6f598c588df531f9fb8467
mateus-henrique-silva <henrique.s@gsuite.com.br>
2022-08-16 13:50:16 -0300

    Implementação e otimização da tela de login

---
+++ my-app/.history/src/Components/formLogin/formLogin_20220816115710.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./formLogin.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const FormLogin = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  return (
+    <div className="boxFather">
+      <div className="box-login">
+        <form className="formLogin">
+          <h1>Acesso</h1>
+          <input
+            type="text"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            value={pass}
+            onChange={(e) => {
+              setPass(e.target.value);
+            }}
+          />
+          <Link to="/">Esqueci minha senha</Link>
+          
+            <input type="submit" />
+      
+          <p style={{textDecoration: 'underline', cursor: 'pointer'}} >Criar conta</p>{" "}
+     
+          <div className="boxCenterCheck">
+          
+              <input type="checkbox" />
+              <label style >Lembre de mim</label>
+          
+          </div>
+       
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormLogin;
